c87744248f572839736df81e900c39d54973dab5
Atharva <121598279+groot34@users.noreply.github.com>
2026-01-29 15:19:42 +0530

    Syling improved

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import { CTA } from "@/components/landing/cta";
+import { HowItWorks } from "@/components/landing/how-it-works";
 import { Features } from "@/components/landing/features";
 import { Hero } from "@/components/landing/hero";
-import { HowItWorks } from "@/components/landing/how-it-works";
 
 export default function Home() {
   return (
